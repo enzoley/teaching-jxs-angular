081150726660eace731e9f611d8094c364c13267
Enzo Leroyer <enzo@dn017246.etu.univ-rennes1.fr>
2024-10-02 08:23:34 +0200

    Q1&Q2

--- pokedemo/src/app/app.module.ts
+++ pokedemo/src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyComponentComponent } from './my-component/my-component.component';
+
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyComponentComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, MyComponentComponent]
 })
 export class AppModule { }
---
+++ pokedemo/src/app/my-component/my-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-component',
+  templateUrl: './my-component.component.html',
+  styleUrl: './my-component.component.css'
+})
+export class MyComponentComponent {
+
+  id: string = '';
+
+}
